23d497ffd2bcdd1b68ce0ffec5cbf3e628685a95
Elise Phan <aliceconnic@gmail.com>
2019-03-14 00:06:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
     return Object.assign({}, driver,{ [key]: value });
-}
+}Object.assign({ eggs: 3 }, { chocolateChips: '1 cup', flour: '2 cups' }, { flour: '1/2 cup' });
