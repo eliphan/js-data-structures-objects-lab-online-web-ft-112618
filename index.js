7defe470ec84095991ff5f1eaf619d2d7dc0227f
Elise Phan <aliceconnic@gmail.com>
2019-03-14 00:05:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
- Object.assign({}}, driver{key:value}});
- 
+       Object.assign({}, driver,{ [key]: value });
+return Object.assign({}, driver, { [key]: value });
 }
